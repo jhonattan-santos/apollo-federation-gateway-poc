ec8f38d4f289ecc7de26f9635c27814eb895c19c
Jhonattan Wesley Santos <jhonattanwesleysantos@gmail.com>
2023-05-08 08:34:11 -0300

    save the state of work

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
+import { IntrospectAndCompose } from '@apollo/gateway';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            { name: 'employee', url: 'http://localhost:3001/graphql' },
+            { name: 'accounts', url: 'http://localhost:3002/graphql' },
+          ],
+        }),
+      },
+      server: {
+        playground: true,
+        cors: true,
+        // plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
